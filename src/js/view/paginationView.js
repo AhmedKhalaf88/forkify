639c380763b77350753b70029d8bbb358cb8faad
Ahmed <ahmed.khalaf.1088@gmail.com>
2024-12-15 01:47:38 +0100

    Refactoring pagination

--- src/js/view/paginationView.js
+++ src/js/view/paginationView.js
@@ -22,51 +22,32 @@ class PaginationView extends View {
     );
 
     // Page 1, and there are other pages
-    if (curPage === 1 && numPages > 1)
+    if (numPages > 1)
       return ` 
         <button data-goto="${
-          curPage + 1
-        }" class="btn--inline pagination__btn--next">
-                <span>Page ${curPage + 1}</span>
-                <svg class="search__icon">
-                <use href="${icons}#icon-arrow-right"></use>
-                </svg>
-        </button>
-          `;
-
-    // Last page
-    if (curPage === numPages && numPages > 1)
-      return `
-        <button data-goto="${
           curPage - 1
-        }" class="btn--inline pagination__btn--prev">
+        }" class="btn--inline pagination__btn--prev ${
+        curPage === 1 && numPages > 1 ? 'hidden' : ''
+      }">
             <svg class="search__icon">
                 <use href="${icons}#icon-arrow-left"></use>
             </svg>
             <span>Page ${curPage - 1}</span>
         </button>
-        `;
-
-    // Other pages
-    if (curPage < numPages)
-      return ` 
-        <button data-goto="${
-          curPage - 1
-        }" class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-                <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span>Page ${curPage - 1}</span>
-        </button>
+        <div class="pagination-page_display">
+          <span>${curPage} / ${numPages}</span>
+        </div>
         <button data-goto="${
           curPage + 1
-        }" class="btn--inline pagination__btn--next">
+        }" class="btn--inline pagination__btn--next ${
+        curPage === numPages && numPages > 1 ? 'hidden' : ''
+      }">
                 <span>Page ${curPage + 1}</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
                 </svg>
         </button>
-      `;
+`;
 
     // Page 1, and there are NO other pages
     return '';
